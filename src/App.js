0c9314df3c468941593a6c0977091a8a12adfd7e
Adrian <adrian_george28@yahoo.com>
2021-11-17 12:54:49 +0000

    Change from Function into Class,and include the DidMount to receive data from fetchata
    And add The props to Dasboard and Header to change  specification based on user id

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import "./App.css";
-import React from "react";
+import React, { Component } from "react";
 import Header from "./Pages/Header";
 import SideBar from "./Pages/SideBar";
 import Greeting from "./Components/Greeting";
 import Dashboard from "./Pages/Dashboard";
+import fetchData from "./Api/api";
 
-/**
- *
- * @returns {JSX.Element}
- */
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <SideBar />
-      <Dashboard />
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.endpoint = this.props.endpoint;
+    this.user = this.props.user;
+    this.state = {
+      items: {},
+      loading: false,
+      error: null,
+    };
+  }
+  componentDidMount() {
+    const { endpoint } = this.props;
+    const { user } = this.props;
+    fetchData(this.user, this.endpoint).then((response) => {
+      this.setState({
+        items: response.data,
+        loading: false,
+      });
+    });
+  }
+  render() {
+    const { error, isLoaded, data } = this.state;
+    return (
+      <div className='App'>
+        <Header user={this.user} />
+        <SideBar />
+        <Dashboard user={this.user} />
+      </div>
+    );
+  }
 }
 
 export default App;
